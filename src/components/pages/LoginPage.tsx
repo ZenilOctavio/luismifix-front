a71e7628e403df273c54d4b34f0e7a4349bca01c
Octavio Zenil <octaviozenil@gmail.com>
2024-05-02 15:52:34 -0700

    Login colors fixed

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -17,13 +17,13 @@ function LoginPage(){
         <div className="flex justify-center w-screen h-screen items-center bg-gradient-to-t to-cyan-500 from-blue-500">
             <div className="relative  h-[600px] flex flex-col items-center justify-center rounded-xl overflow-hidden border shadow-lg shadow-slate-600">
 
-                <div className="p-8 flex justify-center items-center h-full bg-white">
+                <div className="p-8 flex justify-center items-center h-full bg-white dark:bg-gray-800">
                     <div className="mx-auto flex  flex-col justify-evenly space-y-6 sm:w-[25rem] w-full h-full">
                         <div className="flex flex-col justify-center items-center space-y-2 text-center">
                             <div className="rounded-full w-40 h-40 overflow-hidden shadow-md shadow-slate-400">
                                 <img src={logo} alt="logo" className="w-40 h-40 shadow-md"/>
                             </div>
-                        <h1 className="sm:text-3xl text-xl font-bold tracking-tight">
+                        <h1 className="sm:text-3xl text-xl font-bold tracking-tight text-foreground">
                             Iniciar Sesión
                         </h1>
                         <p className="text-muted-foreground sm:text-lg text-base">
