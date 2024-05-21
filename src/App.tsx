a8bb4de95d1c9cfd45755fbb76ca2a306c81e16f
Octavio Zenil <octaviozenil@gmail.com>
2024-05-20 21:35:13 -0700

    Products historical page created

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,30 @@ import LoginPage from './components/pages/LoginPage'
 import HomePage from './components/pages/HomePage'
 import ProvidersPage from './components/pages/ProvidersPage'
 import { ThemeProvider } from './providers/ThemeProvider'
-import { HOME_PAGE_PATHNAME, INVENTORY_PAGE_PATHNAME, PROVIDERS_PAGE_PATHNAME } from './config/constants'
+import { HOME_PAGE_PATHNAME, INVENTORY_PAGE_PATHNAME, PRODUCTS_HISTORICAL_PAGE_PATHNAME, PROVIDERS_PAGE_PATHNAME } from './config/constants'
 import { AuthProvider } from './providers/AuthProvider'
 import { InventoryProviders } from './components/inventory/InventoryProviders'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ProductsHistorical } from './components/pages/ProductsHistorical'
+
+const queryClient = new QueryClient()
 
 function App() {
-
+  
   return (
     <AuthProvider>
       <ThemeProvider defaultTheme= 'dark'>
-        <Router>
-          <Routes>
-            <Route index element = {<LoginPage/>}></Route>
-            <Route path={ HOME_PAGE_PATHNAME } element = {<HomePage/>}></Route>
-            <Route path={ INVENTORY_PAGE_PATHNAME } element = {<InventoryProviders/>}></Route>
-            <Route path={ PROVIDERS_PAGE_PATHNAME } element = {<ProvidersPage/>}></Route>
-          </Routes>
-        </Router>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <Routes>
+              <Route index element = {<LoginPage/>}></Route>
+              <Route path={ HOME_PAGE_PATHNAME } element = {<HomePage/>}></Route>
+              <Route path={ INVENTORY_PAGE_PATHNAME } element = {<InventoryProviders/>}></Route>
+              <Route path={ PROVIDERS_PAGE_PATHNAME } element = {<ProvidersPage/>}></Route>
+              <Route path={ PRODUCTS_HISTORICAL_PAGE_PATHNAME } element = {<ProductsHistorical/>}></Route>
+            </Routes>
+          </Router>
+        </QueryClientProvider>
       </ThemeProvider>
     </AuthProvider>
 
---
+++ src/components/pages/ProductsHistorical.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../NavBar";
+
+export function ProductsHistorical(){
+    return (
+        <div className="w-screen min-w-screen min-h-screen">
+            <NavBar></NavBar>
+            Historical
+        </div>
+    )
+} 
