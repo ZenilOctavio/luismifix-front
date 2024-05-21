a1d5c4c4e5ffc1bfd6d2d6a9257c6c5d3b62d0d1
Octavio Zenil <octaviozenil@gmail.com>
2024-05-21 00:34:56 -0700

    Links button fixed

--- src/components/inventory/ProductsTable.tsx
+++ src/components/inventory/ProductsTable.tsx
@@ -162,7 +162,7 @@ export function ProductsTable({onEditProduct, onEditLinks}: {onEditProduct?: Fun
                             <DropdownMenuTrigger asChild
                             >
                                 <Button 
-                                    className="bg-slate-50 text-slate-600 flex gap-2 dark:bg-slate-800 dark:text-slate-400"
+                                    className="bg-slate-50 text-slate-600 flex gap-2 dark:bg-slate-800 dark:text-slate-400 hover:text-white"
                                 >
                                     Link(s)
                                     <ChevronsUpDown className="w-4 h-4" 
