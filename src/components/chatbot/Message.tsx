be4dfd4da45bafb7ea9bfad37f71e8102cae2cd9
Enrique Rocha <ej.aacount@gmail.com>
2024-11-20 18:16:46 -0700

    chatbot message respects line breaks

--- src/components/chatbot/Message.tsx
+++ src/components/chatbot/Message.tsx
@@ -7,7 +7,12 @@ interface MessageProps {
 
 const Message: React.FC<MessageProps> = ({ speaks, text }) => (
   <div className={`message-container ${speaks === 'bot' ? 'bg-secondary text-foreground' : 'bg-secondary text-primary'} p-2 px-4 flex rounded-lg shadow-md mb-2`}>
-    <p className={`${speaks === 'bot' ? '' : 'ml-auto'}`}>{text}</p>
+    <p
+      className={`${speaks === 'bot' ? '' : 'ml-auto'}`}
+      style={{ whiteSpace: 'pre-line' }}
+    >
+      {text}
+    </p>
   </div>
 );
 
